e0e432e75fe05541d70abdb749dac60d4adc5c5f
Furkan Bayram <furkanbayram@msn.com>
2022-07-09 17:53:34 +0300

    nextjs init with typescript + tailwind

---
+++ components/Seo.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+
+type Props = {
+  title?: string;
+  description?: string;
+};
+
+const Seo: NextPage = ({ title, description }: Props) => {
+  const site = "https://fbayram.com";
+  const router = useRouter();
+  let canonicalURL = "";
+  let path = "";
+  if (router.asPath.slice(-1) === "/") {
+    path = router.asPath.slice(0, -1);
+  } else {
+    path = router.asPath;
+  }
+  canonicalURL = site + path;
+  title = title || "Furkan Bayram";
+  description = description || "I'm a designer and developer from Turkey. ";
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={canonicalURL}
+      openGraph={{
+        url: canonicalURL,
+        title: title,
+        description: description,
+        images: [{ url: "https://fbayram.com/assets/fbayram-og.png" }],
+        site_name: "alllinkone",
+      }}
+      twitter={{
+        handle: "@furkanbayram0",
+        site: "@furkanbayram0",
+        cardType: "summary_large_image",
+      }}
+    />
+  );
+};
+
+export default Seo;
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Seo from "../components/Seo";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Seo />
+      <div className="text-2xl">asd</div>
+    </>
+  );
+};
+
+export default Home;

67bed0fff2b38c517fe849bc6d108f40a7fce0e1
Furkan Bayram <furkanbayram@msn.com>
2022-08-09 17:26:48 +0300

    indent gone

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ const Home: NextPage = () => {
       <div className="grid md:grid-cols-2 items-center gap-12 py-12">
         <div className="order-2 md:order-1">
           <h1 className="text-2xl">Hey 👋</h1>
-          <p className="text-gray-500 mt-4 indent-8">
+          <p className="text-gray-500 mt-4">
             I&apos;m a designer and developer from Turkey. I love being in the
             outdoors; hiking and travelling are some of the things I do to clear
             my mind and relax.
           </p>
-          <p className="text-gray-500 mt-4 indent-8">
+          <p className="text-gray-500 mt-4">
             Being able to both design and develop is a lot of fun. When you have
             an idea, there&apos;s nothing holding you back from building it.
           </p>
